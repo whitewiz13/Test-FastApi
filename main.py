33d8a0e81fb7d80b4321fe07fe15e7be618e4393
whitewiz13 <sandeepthakur130197@gmail.com>
2023-11-26 11:36:18 +0530

    [Update] Req

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ app = FastAPI()
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     try:
-        with open("temp_file.jpg", "wb") as temp_file:
+        with open(file.filename, "wb") as temp_file:
             temp_file.write(file.file.read())
 
         # Use the classifier to classify the uploaded file
-        result = image_classify.classify_img("temp_file.jpg")
+        result = image_classify.classify_img(file.filename)
 
         # Return the result
-        return {"filename": file.filename, "classification_result": result,"isAccepted":result['temp_file.jpg']['safe']*100 >= 89}
+        return {"filename": file.filename, "classification_result": result,"isAccepted":result[file.filename]['safe']*100 >= 89}
     finally:
         # Remove the temporary file
         import os
-        os.remove("temp_file.jpg")
+        os.remove(file.filename)
